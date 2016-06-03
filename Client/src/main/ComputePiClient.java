f49c1d37da128d40884190512ea9e10bd9843523
Natalina Radosevic <nina.radosevic@gmail.com>
2016-06-03 10:12:20 +0200

    calculate pi from all server hints

--- Client/src/main/ComputePiClient.java
+++ Client/src/main/ComputePiClient.java
@@ -18,20 +18,23 @@ public class ComputePiClient {
 			// Number of "tropfenZahl" (First Argument after Servers IP)
 			int tropfenZahl= Integer.parseInt(args[server+1]);
 			int tropfenViertelkreis = 0;
+			int gesamtTreffer = 0;
 			
 			for(int i = 0 ; i < server; i++){ 
 				String name = "//" + args[1+i] + "/ComputePi";
 				Registry myRegistry = LocateRegistry.getRegistry();
 				ComputePiRemote computePiRemote = (ComputePiRemote) myRegistry.lookup(name);
 				
-				//Calculate tropfenViertelkreis
+				//Calculate hints per Server
                 tropfenViertelkreis = computePiRemote.computePi(tropfenZahl);
-
+                //Calculate hints all Server
+                gesamtTreffer = gesamtTreffer + tropfenViertelkreis;
+                
                 System.out.println("Server " + (i + 1) + " hat "  + tropfenViertelkreis + " Treffer.");
-            }
-              
-            BigDecimal pi = new BigDecimal(4*(double) tropfenViertelkreis/tropfenZahl);
-            System.out.println("Tropfenanzahl: "+tropfenZahl+", Tropfen im Viertelkreis aller Server: "+tropfenViertelkreis+"."+"\nDie Naeherung fuer Pi: "+pi);
+            }         
+		
+            BigDecimal pi = new BigDecimal(4*(double) gesamtTreffer/(tropfenZahl*server));
+            System.out.println("Tropfenanzahl pro Server: "+tropfenZahl+"\nTropfen im Viertelkreis aller Server: "+gesamtTreffer+"."+"\nDie Naeherung fuer Pi: "+pi);
             
 		} catch (Exception e) {
 			System.err.println("ComputePiClient exception: " + e.getMessage());
